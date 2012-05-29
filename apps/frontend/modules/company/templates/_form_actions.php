6dc0149f29ebf5b5decb8d35e6b7afe069b65b55
Rober Martín H <rmh.dev@gmail.com>
2012-05-29 17:57:09 +0200

    Improving html

--- apps/frontend/modules/company/templates/_form_actions.php
+++ apps/frontend/modules/company/templates/_form_actions.php
@@ -4,7 +4,7 @@
         <?php echo $form->isNew() ? 'Create' : 'Update'; ?>
     </button>
 
-    <button type="submit" id="button_save_and_add" name="save_and_add" class="btn info">
+    <button type="submit" id="button_save_and_add" name="save_and_add" class="btn success">
         <?php echo $form->isNew() ? 'Create and add new' : 'Update and add new'; ?>
     </button>
 </div>
--- apps/frontend/modules/company/templates/showSuccess.php
+++ apps/frontend/modules/company/templates/showSuccess.php
@@ -3,7 +3,7 @@
 <div class="row">
     <div class="span16">
         <?php include_partial('company/show', array('company' => $company)); ?>
-
+        
         <?php echo link_to('Back to list', '@company', array('class' => 'btn ')); ?>
         <?php echo link_to('Edit', url_for('company_edit', $company), array('class' => 'btn info', 'id' => 'button_edit')); ?>
     </div>
